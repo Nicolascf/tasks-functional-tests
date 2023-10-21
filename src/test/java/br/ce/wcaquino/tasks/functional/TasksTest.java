ceaee224f9c2eabee7bb4815c1d2c0e8adedda7f
Nicolas Castro <nicolas_castro123@hotmail.com>
2023-10-20 21:49:39 -0300

    Atualizando codigo para rodar com selenium grid atualizado

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -6,20 +6,21 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 	
 	public WebDriver acessarAplicacao() throws MalformedURLException {
-//			WebDriver driver = new ChromeDriver();
-			DesiredCapabilities cap = DesiredCapabilities.chrome();
-			WebDriver driver = new RemoteWebDriver(new URL("http://192.168.100.52:4444/wd/hub"), cap);
-			driver.navigate().to("http://192.168.100.52:8001/tasks");
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			return driver;
+	    ChromeOptions options = new ChromeOptions();
+	    WebDriver driver = new RemoteWebDriver(new URL("http://192.168.100.52:4444/wd/hub"), options);
+	    driver.navigate().to("http://192.168.100.52:8001/tasks");
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    return driver;
 	}
 	
 	@Test
